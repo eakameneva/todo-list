2c604977ef480e6789ae75c6ec3e054ccf2a505e
eakameneva <119859011+eakameneva@users.noreply.github.com>
2024-06-20 22:13:02 +0200

    Update AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import NewTaskForm from './NewTaskForm'
-import './appHeader.css'
+import './AppHeader.css'
 
 function AppHeader({ onItemAdded }) {
   return (
